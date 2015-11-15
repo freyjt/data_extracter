9445047f61ab9fd38787dadcb2547632e2cb49c4
Joshua Frey <freyjt@gmail.com>
2015-11-15 18:46:04 -0500

    Created main.js, found it difficult to work with phantom requires, pivoting to node superstructure.

--- getGameLog.js
+++ getGameLog.js
@@ -1,93 +1,98 @@
+function getGameLog(playerId) {
 
-var page = require('webpage').create();
+    var page = require('webpage').create();
+    var url  = 'http://espn.go.com/nba/player/gamelog/_/id/' + playerId;
 
-page.onConsoleMessage = function(msg) {
-    console.log(msg);
-};     
 
-page.open('http://espn.go.com/nba/player/gamelog/_/id/1966/', function(status) {
+    page.onConsoleMessage = function(msg) {
+        console.log(msg);
+    };     
 
-    // var content = page.content;
-    //the evaluate is where we can use some DOM js
-    table      = page.evaluate( function() {
-        // var string = document.getElementsByClassName("tablehead")[0].innerHTML;
-        // string = string.replace(/>/g, "> \n");
-        // string = string.replace(/<\/tr>/g, "");
-        date     = document.getElementsByClassName("tablesm")[0].value
-        
-        //abuses table classing of even and oddrows to get all games
-        var oddGames  = document.getElementsByClassName("oddrow");
-        var evenGames = document.getElementsByClassName("evenrow")
-        var next;
-        var dateStr, playAgainst, gameScore, timePlayed;
-        var gamesObj  = {};
-        for( i = 0; i < oddGames.length; i++) {
-            //seed it up
-            next = oddGames[i].firstChild;
-            console.log(next.innerHTML);
-            if(next.innerHTML != 'Averages') {
-                ///OMG just iterate into an array, then dump the array
-                //   when you assign the json
-                //divide current row
-                dateStr     = next.innerHTML;
-                next        = next.nextSibling;
-                // todo this breaks on heading rows
-                playAgainst = next.getElementsByTagName('a')[1].innerHTML;
-                
-                next        = next.nextSibling;
-                gameScore   = next.getElementsByTagName('a')[0].innerHTML;
+    page.open(url, function(status) {
 
-                next        = next.nextSibling;
-                timePlayed  = next.innerHTML;
-                next        = next.nextSibling;
-                
-                twoAttempt  = next.innerHTML;
-                next        = next.nextSibling;
-                twoPercent  = next.innerHTML;
-                next        = next.nextSibling;
+        // var content = page.content;
+        //the evaluate is where we can use some DOM js
+        table      = page.evaluate( function() {
+            date       = document.getElementsByClassName("tablesm")[0].value
+            
+            //abuses table classing of even and oddrows to get all games
+            var oddGames  = document.getElementsByClassName("oddrow");
+            var evenGames = document.getElementsByClassName("evenrow")
+            var next;
+            var dateStr, playAgainst, gameScore, timePlayed;
+            var gamesObj  = {};
+            for( i = 0; i < oddGames.length; i++) {
+                //seed it up
+                next = oddGames[i].firstChild;
+                console.log(next.innerHTML);
+                if(next.innerHTML != 'Averages') {
+                    ///OMG just iterate into an array, then dump the array
+                    //   when you assign the json
+                    //divide current row
+                    dateStr     = next.innerHTML;
+                    next        = next.nextSibling;
+                    // todo this breaks on heading rows
+                    playAgainst = next.getElementsByTagName('a')[1].innerHTML;
+                    
+                    next        = next.nextSibling;
+                    gameScore   = next.getElementsByTagName('a')[0].innerHTML;
 
-                fgMade      = parseInt(twoAttempt);
-                fgTried     = parseInt( (twoAttempt.substr(fgMade.toString().length + 1)) )
-                console.log(" L " + fgMade.length)
-                console.log(twoAttempt + " : " + fgMade);
-                console.log(fgTried);           threeAttempt= next.innerHTML;
-                next        = next.nextSibling;
-                threePercent= next.innerHTML;
-                next        = next.nextSibling;
+                    next        = next.nextSibling;
+                    timePlayed  = next.innerHTML;
+                    next        = next.nextSibling;
+                    
+                    twoAttempt  = next.innerHTML;
+                    next        = next.nextSibling;
+                    twoPercent  = next.innerHTML;
+                    next        = next.nextSibling;
 
-                freeAttempt = next.innerHTML;
-                next        = next.nextSibling;
-                freePercent = next.innerHTML;
-                next        = next.nextSibling;
+                    fgMade      = parseInt(twoAttempt);
+                    fgTried     = parseInt( (twoAttempt.substr(fgMade.toString().length + 1)) )
+                    console.log(" L " + fgMade.length)
+                    console.log(twoAttempt + " : " + fgMade);
+                    console.log(fgTried);           threeAttempt= next.innerHTML;
+//where is three tried
+                    next        = next.nextSibling;
+                    threePercent= next.innerHTML;
+                    next        = next.nextSibling;
 
-                rebound     = next.innerHTML;
-                next        = next.nextSibling;
-                assists     = next.innerHTML;
-                next        = next.nextSibling;
-                blocks      = next.innerHTML;
-                next        = next.nextSibling;
-                steals      = next.innerHTML;
-                next        = next.nextSibling;
-                fouls       = next.innerHTML;
-                next        = next.nextSibling;
-                turnovers   = next.innerHTML;
-                next        = next.nextSibling;
-                points      = next.innerHTML;
-                gamesObj[dateStr] = {
-                    opponent: playAgainst,
-                    score:    gameScore,
-                    minutes:  timePlayed
+                    freeAttempt = next.innerHTML;
+                    next        = next.nextSibling;
+
+
+                    freePercent = next.innerHTML;
+                    next        = next.nextSibling;
+
+                    rebound     = next.innerHTML;
+                    next        = next.nextSibling;
+                    assists     = next.innerHTML;
+                    next        = next.nextSibling;
+                    blocks      = next.innerHTML;
+                    next        = next.nextSibling;
+                    steals      = next.innerHTML;
+                    next        = next.nextSibling;
+                    fouls       = next.innerHTML;
+                    next        = next.nextSibling;
+                    turnovers   = next.innerHTML;
+                    next        = next.nextSibling;
+                    points      = next.innerHTML;
+                    gamesObj[dateStr] = {
+                        opponent: playAgainst,
+                        score:    gameScore,
+                        minutes:  timePlayed,
+                        twoMade:  fgMade,
+                        twoTried: fgTried,
+
+                    }
 
                 }
-
             }
-        }
-        console.log(date);
-        return oddGames;
-    } );
+            console.log(date);
+            return oddGames;
+        } );
 
-   // console.log(table);
-    phantom.exit();
+       // console.log(table);
+        phantom.exit();
 
-});
-
+    });
+}
---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+function Main( ) {
+    var page    = require('webpage').create();
+    var gameLog = page.includeJS('getGameLog.js');
+
+    var testId  = '1966'//lebron james
+
+
+}
+
+Main( );
