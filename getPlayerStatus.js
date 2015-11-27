d7ad30c9e3ca8141a26ffb230ac164673c917159
Joshua Frey <freyjt@gmail.com>
2015-11-27 01:28:05 -0500

    Wrote script to query player status page, actual commit ;).

---
+++ getPlayerStatus.js
@@ -0,0 +1,38 @@
+
+
+
+//
+function getPlayerStatus( playerId ) {
+
+    //make sure you deal with typechecking on playerId in main ;)
+    var url  = "http://espn.go.com/nba/player/_/id/" + playerId;
+    var page = require('webpage').create();
+console.log(url);
+    page.open( url, function(status) {
+        if(status == 'fail')
+            console.log("Error, could not collect status on player: " + playerId + ". Webpage unavailable.");
+        else {
+            status = page.evaluate( function( ) {
+
+                stat = document.getElementById('player-status-main');
+                if(typeof(stat) !== 'undefined') {
+                    return 'out';
+                } else {
+                    return 'active';
+                }
+            });
+            console.log(status);
+        }
+        phantom.exit();
+    });
+}
+
+function main( ) {
+
+    var system   = require('system');
+    console.log(system.args[1]);
+    playerId     = parseInt(system.args[1]);
+
+    getPlayerStatus( playerId );
+}
+main();
