f72162ec350e1256fe8d541135d8f2bcb8808dbb
Joshua Frey <freyjt@gmail.com>
2016-09-17 15:07:13 -0400

    General table to csv working; One TODO

--- callWithThrow.js
+++ callWithThrow.js
@@ -2,7 +2,7 @@
 //Gain visibility into the call you're trying to make
 // Throw if element is not in list
 exports.call = function(obj, call, parm_list) {
-  if(Object.keys(obj).includes(call) {
+  if(Object.keys(obj).includes(call)) {
     return obj[call](parm_list);
   } else { throw "Illegal Argument Exception"; }
 }
--- getTable.js
+++ getTable.js
@@ -4,20 +4,36 @@
 //  There can be colgroups
 exports.getTable = function(page, url, tableName) {
   console.log("Called getTable for " + url + " " + tableName);
+  var ex = require(phantom.libraryPath + "/callWithThrow.js");
   page.open(url, function(status) {
     console.log("Called page.open");
     if(status == 'fail') {
       console.log("Unable to open webpage");
     } else { console.log("Opened web page"); }
     //page.evaluate goes here!
-    table_csv =  page.evaluate( function(tableName) {
-      csv_str = '';
-      tableau = document.getElementsByTagName(tableName);
-      csv_str += tableau[0].innerHTML;
+    table_csv =  page.evaluate( function(tableName, oth) {
+      csv_str = tableName + " " + oth +  "\n";
+      query_str = 'table.' + tableName;
+      tableau = document.querySelectorAll(query_str); 
+      csv_str += Object.keys(tableau) + "\n";
+      csv_str += Object.keys(tableau[0]) + "\n";
+      for(var i = 0; i < tableau.length; i++) {
+        var rowe = tableau[i]['rows'];
+        for(var j = 0; j < rowe.length; j++) {
+          var sell = tableau[i]['rows'][j]['cells'];
+          for(var k = 0; k < sell.length; k++) {
+            // @TODO collect attributes 
+            var here = sell[k];
+            csv_str += here.textContent;
+            if(k < sell.length - 1) csv_str += ",";
+          }
+          csv_str += "\n";
+       
+        }
+      }
       return csv_str;
-    }, tableName);
-    console.log(table_csv);
+    }, tableName, 'othervar');
+    console.log("csv " + table_csv + " ::");
   });
 };
-
 
